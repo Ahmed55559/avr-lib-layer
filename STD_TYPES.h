291dafb26f566120220b8d8ace426d62309615f5
Ahmed Ashraf <139428852+Ahmed55559@users.noreply.github.com>
2026-08-20 21:56:13 +0300

    Update STD_TYPES.h

--- STD_TYPES.h
+++ STD_TYPES.h
@@ -1,6 +1,5 @@
 // ========================================= STD_TYPES.h =========================================
 // Auther: Eng. Ahmed Ashraf
-// SWC: DIO/GPIO
 // Layer: LIB
 // Description: Standard Types Header File
 // ===============================================================================================
@@ -25,19 +24,3 @@ typedef double f64;
 
 #endif
 
-// --- ATmega328P Type Size Report ---
-// -----------------------------------
-// unsigned char:      1
-// unsigned short:     2
-// unsigned int:       2
-// unsigned long:      4
-// unsigned long long: 8
-// --- Signed Types ---
-// signed char:        1
-// signed short:       2
-// signed int:         2
-// signed long:        4
-// signed long long:   8
-// --- Floating Point ---
-// float:              4
-// double:             4
